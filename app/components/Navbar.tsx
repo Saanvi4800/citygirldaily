5c13c86fbbcb545601b651e0aedcf543980d6ed3
Aanya Kungwani <aanyak820@gmail.com>
2025-11-15 12:36:40 -0500

    chnge

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav style={{
+      display: "flex",
+      padding: "20px",
+      background: "#ffb6d9",
+      borderBottom: "2px solidrgb(170, 39, 107)"
+    }}>
+      <Link href="/">home</Link>
+      <Link href="/gallery">gallery</Link>
+      <Link href="/subway">subway</Link>
+      <Link href="/itinerary">itinerary</Link>
+    </nav>
+  );
+}
---
+++ app/gallery/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", textAlign: "center" }}>
+      <h1>NYC Travel Guide</h1>
+      <p>Welcome to my website built with React + Next.js!</p>
+    </main>
+  );
+}

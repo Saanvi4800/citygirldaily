b6470141291347b52f7af18a4b4491b4f7919237
Annie Ma <annie9ma@gmail.com>
2025-11-15 17:36:58 -0500

    trying

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,16 +1,19 @@
 "use client";
 import { useState } from "react";
-import Navbar from "@/components/Navbar";
+// import Navbar from "@/components/Navbar";
+import CircularGallery from "./CircularGallery";
 
 export default function GalleryPage() {
   return (
     <main>
       <div>
-      <Navbar />
+      {/* <Navbar /> */}
       <div style={{ padding: "40px" }}>
         <h1>gallery</h1>
-        <p>rotating gallery here</p>
-      </div>
+          <div style={{ height: '600px', position: 'relative' }}>
+            <CircularGallery bend={3} textColor="#ffffff" borderRadius={0.05} scrollEase={0.02}/>
+          </div>
+        </div>
       </div>
     </main>
   );

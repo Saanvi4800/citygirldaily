b2d1b86ffb87f229d1b316126eb93b51bcb6d5b2
Annie Ma <annie9ma@gmail.com>
2025-11-15 17:23:50 -0500

    fix periods

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,12 +1,16 @@
+"use client";
+import { useState } from "react";
 import Navbar from "@/components/Navbar";
 
 export default function GalleryPage() {
   return (
     <main>
+      <div>
       <Navbar />
       <div style={{ padding: "40px" }}>
-        <h1>Gallery 📸</h1>
-        <p>Rotating cute gallery will go here!</p>
+        <h1>gallery</h1>
+        <p>rotating gallery here</p>
+      </div>
       </div>
     </main>
   );

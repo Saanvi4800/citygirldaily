7487177c74134573b720fb4b3525cc46c89a975a
Saanvi4800 <Saanvi4800@gmail.com>
2025-11-15 22:58:17 -0500

    added more places

--- app/maps/page.tsx
+++ app/maps/page.tsx
@@ -7,7 +7,12 @@ import { useEffect } from 'react';
 const locations = [
   { lat: 40.73, lng: -74, title: "Rosemary's Resturant" },
   { lat: 40.7580, lng: -73.9855, title: "Times Square" },
-  { lat: 40.7309, lng: -73.9973, title: "Washington Square Park" }
+  { lat: 40.7309, lng: -73.9973, title: "Washington Square Park" },
+  { lat: 40.7826, lng: -73.9656, title: "Central Park"},
+  { lat: 40.7794, lng: -73.9631, title: "The Met Museum" },
+  { lat: 40.759, lng: -73.985, title: "Broadway Theaters" },
+  { lat: 40.723, lng: -74.000, title: "SoHo Shopping District" },
+  { lat: 40.44, lng: -74.0022, title: "Chelsea Market" },
 ];
 
 // Component to auto-fit map to all markers
@@ -30,7 +35,7 @@ export default function MapsPage() {
 
   return (
     <APIProvider apiKey={apiKey || ""}>
-      <div style={{ height: "100vh", width: "100%",}}>
+      <div style={{ height: "90vh", width: "100%",}}>
         <Map defaultZoom={12} defaultCenter={locations[0]}>
           {locations.map((loc, idx) => (
             <Marker
